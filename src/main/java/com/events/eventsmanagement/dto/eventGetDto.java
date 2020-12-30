ae637148a517c30cfcfaead2e05569cd9d7131dd
Anas CHERIET <anascheriet@hotmail.com>
2020-12-30 20:00:47 +0100

    Get user info in eventObject

---
+++ src/main/java/com/events/eventsmanagement/dto/eventGetDto.java
@@ -0,0 +1,17 @@
+package com.events.eventsmanagement.dto;
+
+import com.events.eventsmanagement.models.Event;
+import lombok.Data;
+
+@Data
+public class eventGetDto {
+    private Event event;
+    private int creatorId;
+    private String creatorName;
+
+    public eventGetDto(Event event, int id, String displayName) {
+        this.event = event;
+        this.creatorId = id;
+        this.creatorName = displayName;
+    }
+}
